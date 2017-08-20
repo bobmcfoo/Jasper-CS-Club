60f92eb27375714cb31559c13836c1f7480db40e
kmeng01 <kevin.meng.01@gmail.com>
2017-08-20 18:35:28 -0500

    add comments to HelloWorld.java

--- Exercise Files/Meeting 1 - Introduction/HelloWorld.java
+++ Exercise Files/Meeting 1 - Introduction/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); //Use the standard out stream to print a string to the console.
     }
 }
