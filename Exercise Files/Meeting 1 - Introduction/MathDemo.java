4dd9db8c2b57179dbf6509eebea538336db5baf5
kmeng01 <kevin.meng.01@gmail.com>
2017-08-20 18:48:51 -0500

    add mathdemo.java

---
+++ Exercise Files/Meeting 1 - Introduction/MathDemo.java
@@ -0,0 +1,23 @@
+public class MathDemo {
+    public static void main(String[] args) {
+        int A = 4; //New number
+        int B = 9; //Another new number
+
+        //4 Function
+
+        int AplusB = A + B; //Add numbers
+        int AminusB = A - B; //Subtract numbers
+        int AdivideB = A / B; //Divide numbers
+        int AmultiplyB = A * B; //Multiply numbers
+
+        //More stuff
+
+        int squareRootA = Math.sqrt(A);
+        int absoluteValueA = Math.abs(B);
+        int aToPowerOfB = Math.pow(A, B);
+
+        //What about Pi?
+
+        int Pi = Math.PI;
+    }
+}
