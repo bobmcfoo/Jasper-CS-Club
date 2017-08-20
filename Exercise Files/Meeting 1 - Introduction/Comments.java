c4588fb0baccf3e3d193d73765884987928eeb22
kmeng01 <kevin.meng.01@gmail.com>
2017-08-20 18:38:10 -0500

    add comments.java

---
+++ Exercise Files/Meeting 1 - Introduction/Comments.java
@@ -0,0 +1,10 @@
+//This is a comment! Comments are ignored by Java; they don't do anything to your code. They're simply for you to make notes to yourself. You can put comments anywhere, from here
+public class HelloWorld {
+    //to here
+    public static void main(String[] args) {
+        //to here
+        System.out.println("Hello World!");
+    }
+    //to here
+}
+//and even here!
