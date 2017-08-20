f4e8c47f110be59d6de2a30725e89d665c089a78
kmeng01 <kevin.meng.01@gmail.com>
2017-08-20 18:42:14 -0500

    updadted consoleio.java2

--- Exercise Files/Meeting 1 - Introduction/ConsoleIO.java
+++ Exercise Files/Meeting 1 - Introduction/ConsoleIO.java
@@ -2,7 +2,7 @@ import java.util.*; //You must add this to your code for Java to know where to a
 
 public class ConsoleIO {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); //Create new Scanner object. You can use this Scanner to get numbers, letters, and symbols from the console!
+        Scanner sc = new Scanner(System.in); //Create new Scanner object cannled "sc". You can use this Scanner to get numbers, letters, and symbols from the console!
         int scannedInteger = sc.nextInt(); //Create an Integer and scan into it the number entered into the console.
         System.out.println("You just scanned the number: "+scannedInteger); //Use the standard out stream to print the number you just scanned out the the console.
         sc.close(); //Close the scanner object - this is simply good practice to avoid memory leakage.
