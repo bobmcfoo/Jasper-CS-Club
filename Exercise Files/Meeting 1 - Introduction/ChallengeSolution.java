a8f64f873aa7adbfd81fa7309ff3217110c5f8e4
Christopher Zhou <cxqzhou88@gmail.com>
2017-08-20 19:36:52 -0500

    Update ChallengeSolution.java
    format changes

--- Exercise Files/Meeting 1 - Introduction/ChallengeSolution.java
+++ Exercise Files/Meeting 1 - Introduction/ChallengeSolution.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class ChallengeSolution {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int A=sc.nextInt();
-        int B=sc.nextInt();
-        double solution=Math.log10((8*Math.pow(A, 3))/(B-23)*Math.PI);
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        double solution = Math.log10((8 * Math.pow(A, 3))/(B - 23) * Math.PI);
         System.out.println(solution);
     }
 }
