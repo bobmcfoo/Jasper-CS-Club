ba9eac46e3fb6497843c6955c7c8f17ec306d9a1
kmeng01 <kevin.meng.01@gmail.com>
2017-08-20 20:22:05 -0500

    comment solution

--- Exercise Files/Meeting 2 - Java Math/ChallengeSolution.java
+++ Exercise Files/Meeting 2 - Java Math/ChallengeSolution.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 public class ChallengeSolution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        double solution = Math.log10((8 * Math.pow(A, 3)) / (B - 23) * Math.PI);
-        System.out.println(solution);
+        Scanner sc = new Scanner(System.in); //Create new Scanner object to get numbers in
+        int A = sc.nextInt(); //Get A
+        int B = sc.nextInt(); //Get B
+        double solution = Math.log10((8 * Math.pow(A, 3)) / (B - 23) * Math.PI); //Calculate answer and insert into double variable
+        System.out.println(solution); //Print solution
+        sc.close(); //Close Scanner
     }
 }
